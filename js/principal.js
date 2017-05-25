9f216a4b5f30024fb86318fed99e9b13a69187e9
Bruno Silva <bruno.silva890@etec.sp.gov.br>
2017-05-25 18:54:16 -0300

    Primeiro envio do projeto

---
+++ js/principal.js
@@ -0,0 +1,24 @@
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+
+  var peso = pacientes[i].querySelector(".info-peso").textContent;
+
+  var altura = pacientes[i].querySelector(".info-altura").textContent;
+
+  var tdImc = pacientes[i].querySelector(".info-imc");
+
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  if ((peso <= 0 || peso >= 1000) && (altura <= 0 || altura >= 3.00)) {
+      tdImc.textContent = "Peso/Altura inválido(s)";
+      pacientes[i].classList.add("campo-invalido");
+  }
+  else{
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
+
+}
