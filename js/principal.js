4dae4c0e2ca075ff2aa0309bb917cd02f1949e3d
Bruno Silva <bruno.silva890@etec.sp.gov.br>
2017-05-30 08:50:09 -0300

    conclusão aula 04 - Eventos, formulários e criando elementos

--- js/principal.js
+++ js/principal.js
@@ -4,9 +4,7 @@ var pacientes = document.querySelectorAll(".paciente");
 for(var i = 0; i < pacientes.length; i++){
 
   var peso = pacientes[i].querySelector(".info-peso").textContent;
-
   var altura = pacientes[i].querySelector(".info-altura").textContent;
-
   var tdImc = pacientes[i].querySelector(".info-imc");
 
   var pesoEhValido = true;
@@ -22,3 +20,42 @@ for(var i = 0; i < pacientes.length; i++){
   }
 
 }
+
+// -------- FORMULÁRIO
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event){
+
+	//event.preventDefault();
+
+	var form = document.querySelector("#form-adiciona");
+
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
+
+});
