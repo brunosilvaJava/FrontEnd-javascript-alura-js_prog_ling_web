773a1fe9eaf3247f9d069f4778a26d3864c65161
Bruno Silva <bruno.silva890@etec.sp.gov.br>
2017-06-01 09:09:03 -0300

    05-03  criando funções

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,22 +1,7 @@
+function calculaImc(peso, altura) {
 
-var pacientes = document.querySelectorAll(".paciente");
-
-for(var i = 0; i < pacientes.length; i++){
-
-  var peso = pacientes[i].querySelector(".info-peso").textContent;
-  var altura = pacientes[i].querySelector(".info-altura").textContent;
-  var tdImc = pacientes[i].querySelector(".info-imc");
-
-  var pesoEhValido = true;
-  var alturaEhValida = true;
-
-  if ((peso <= 0 || peso >= 1000) && (altura <= 0 || altura >= 3.00)) {
-      tdImc.textContent = "Peso/Altura inválido(s)";
-      pacientes[i].classList.add("campo-invalido");
-  }
-  else{
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc.toFixed(2);
-  }
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
 
 }
--- js/form.js
+++ js/form.js
@@ -25,14 +25,37 @@ botaoAdicionar.addEventListener("click", function(event){
   pesoTd.textContent = peso;
   alturaTd.textContent = altura;
   gorduraTd.textContent = gordura;
+  imcTd.textContent = calculaImc(peso, altura);
 
   pacienteTr.appendChild(nomeTd);
   pacienteTr.appendChild(pesoTd);
   pacienteTr.appendChild(alturaTd);
   pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
 
   var tabela = document.querySelector("#tabela-pacientes");
 
   tabela.appendChild(pacienteTr);
 
 });
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+
+  var peso = pacientes[i].querySelector(".info-peso").textContent;
+  var altura = pacientes[i].querySelector(".info-altura").textContent;
+  var tdImc = pacientes[i].querySelector(".info-imc");
+
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  if ((peso <= 0 || peso >= 1000) && (altura <= 0 || altura >= 3.00)) {
+      tdImc.textContent = "Peso/Altura inválido(s)";
+      pacientes[i].classList.add("campo-invalido");
+  }
+  else{
+    tdImc.textContent = calculaImc(peso, altura);
+  }
+
+}
